9b5bf7836eadc8b07340d7f7898911c5bd9e3d27
estefaniamoncada <estefaniamr716@gmail.com>
2020-11-03 10:24:23 -0600

    otros cambios en los test

--- test/functions.spec.js
+++ test/functions.spec.js
@@ -1,19 +1,19 @@
 import MockFirebase from 'mock-cloud-firestore';
+
+const fixtureData = {
+  __collection__: {
+    publications: {
+      __doc__: {
+        abc1d: {
+          note: 'hola viajero'
+        },
+      },
+    },
+  },
+};
+
+global.firebase = new MockFirebase(fixtureData, { isNaiveSnapshotListenerEnabled: true, });
 import { crud } from '../src/firebase/funcionesGenerales.js';
-
-const firebasemock = require('firebase-mock');
-
-const mockauth = new firebasemock.MockFirebase();
-const mockfirestore = new firebasemock.MockFirestore();
-mockfirestore.autoFlush();
-mockauth.autoFlush();
-
-global.firebase = firebasemock.MockFirebaseSdk(
-  //  use null if your code does not use RTDB
-  () => null,
-  () => mockauth,
-  () => mockfirestore,
-);
 
 describe('Agregar post', () => {
   it('should add post', (done) => {
@@ -39,71 +39,9 @@ describe('Agregar post', () => {
       });
   });
 });
-/* describe.only('Editar post', () => {
-  it.only('should edit post', (done) => {
-    const dataOrigin = {
-      id: 'cualquierid',
-      comentario: 'comentario',
-    }
-    const data = {
-      comentario: 'nuevo comentario',
-    }
-    // en firestore existe un post con el comentario `comentario`
-    firebase.firestore().collection('posts').get()
-      .then((result) => {
-        console.log(result.data);
-        expect(Object.values(result.data).filter((p) => p.comentario === data.comentario).length).toBe(1);
-        return crud.editar(dataOrigin.id, data)      
-      })
-      .then((docRef) => {
-        // en firestore existe un post con el comentario `comentario`
-        return firebase.firestore().collection('posts').get()
-      })
-      .then((result2) => {
-        expect(Object.values(result2.data).filter((p) => p.comentario !== p.comentario).length).toBe(1);
-        done();
-      });
-  });
-});
-describe('Eliminar post', () => {
-  it('should delete post', (done) => {
-    const data = {
-      comentario: 'comentario',
-      displayName: 'name',
-      photoURL: 'http://',
-    }
-    // en firestore  no existe un post con el comentario `comentario`
-    firebase.firestore().collection('posts').get()
-      .then((result) => {
-        console.log(result.data);
-        expect(Object.values(result.data).filter((p) => p.comentario === data.comentario).length).toBe(0);
-        return crud.eliminar(data)      
-      })
-      .then((docRef) => {
-        // en firestore existe un post con el comentario `comentario`
-        return firebase.firestore().collection('posts').get()
-      })
-      .then((result2) => {
-        expect(Object.values(result2.data).filter((p) => data === undefined).length).toBe(0);
-        done();
-      });
-  });
-}); */
-const fixtureData = {
-  __collection__: {
-    publications: {
-      __doc__: {
-        abc1d: {
-          note: 'hola viajero'
-        },
-      },
-    },
-  },
-};
-global.firebase = new MockFirebase(fixtureData, { isNaiveSnapshotListenerEnabled: true, }); 
-
-describe.only('Edit a post', () => {
-  it.only('Should edit a post', (done) => {
+ 
+describe('Edit a post', () => {
+  it('Should edit a post', (done) => {
     return crud.editar('abc1d', 'hola viajero, gracias por venir')
       .then(() => firebase.firestore().collection('posts').get()(
         (data) => {
@@ -128,3 +66,8 @@ describe('Delete a post', () => {
   });
 });
 
+/* describe('Edit a post', () => {
+  it('Should edit a post', () => {
+    const mockEdit = jest.fn();
+  });
+}); */
